e9a0af71a84eb4818157c5670c0a6f130bc3cf6b
Stefan Schmidt <stefan.schmidt@kaeuferportal.de>
2015-08-19 19:01:51 +0200

    add title and headline to Media::Text

--- dpa_parser/result/media/text.php
+++ dpa_parser/result/media/text.php
@@ -11,6 +11,14 @@ class Text extends \DPAParser\Result\Media {
     return $this->id_string_part(1);
   }
 
+  public function title() {
+    return $this->xml()->head->title;
+  }
+
+  public function headline() {
+    return $this->body()->{'body.head'}->hedline->hl1;
+  }
+
   private function id_string_part($index) {
     $id_string = $this->xml()->head->docdata->{'doc-id'}['id-string'];
     return explode(':', $id_string)[$index];
