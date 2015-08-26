ce1b13ded2ff8abf21e5106f0de0dde4016bd2e7
Stefan Schmidt <stefan.schmidt@kaeuferportal.de>
2015-08-26 20:20:05 +0200

    Make require dir relative

--- php-dpa-parser.php
+++ php-dpa-parser.php
@@ -1,18 +1,20 @@
 <?php
 require 'php-dpa-parser/parser.php';
 
-foreach (glob('php-dpa-parser/result/*.php') as $file) {
+$dirname = DIRNAME(__FILE__);
+
+foreach (glob("{$dirname}/php-dpa-parser/result/*.php") as $file) {
   require $file;
 }
 
-foreach (glob('php-dpa-parser/result/media/*.php') as $file) {
+foreach (glob("{$dirname}/php-dpa-parser/result/media/*.php") as $file) {
   require $file;
 }
 
-foreach (glob('php-dpa-parser/result/content/*.php') as $file) {
+foreach (glob("{$dirname}/php-dpa-parser/result/content/*.php") as $file) {
   require $file;
 }
 
-foreach (glob('php-dpa-parser/result/content/media/*.php') as $file) {
+foreach (glob("{$dirname}/php-dpa-parser/result/content/media/*.php") as $file) {
   require $file;
 }
